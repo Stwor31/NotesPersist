9ec14cd5a1d06db1262c07b1dbf4189a339a393b
Steph W <stephanie.worladge@rmit.edu.au>
2025-02-03 18:50:58 +1100

    ContentView Initial commit

--- NotesPersist/ContentView.swift
+++ NotesPersist/ContentView.swift
@@ -8,15 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var newNoteContent = ""
+    var noteManager = NoteManager(savedNotes: [])
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Spacer()
+                Text ("What did you learn today ? ")
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                TextEditor(text: $newNoteContent)
+                    .frame(height: 300)
+                    .padding()
+                    .overlay(RoundedRectangle(cornerRadius: 8).stroke(Color.gray))
+                Button("Save note") {
+                    noteManager.saveNote(content: newNoteContent)
+                    newNoteContent = ""
+                }
+                .padding()
+                .buttonStyle(.borderedProminent)
+                
+                List {
+                    ForEach (noteManager.savedNotes) { note in
+                        VStack {
+                            Text (note.content)
+                                .font(.headline)
+                            Text (note.formattedDate)
+                                .font(.subheadline)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                    .onDelete(perform: noteManager.deleteNote)
+                }
+                .navigationTitle("Notes")
+                
+            }
+            .padding()
         }
-        .padding()
     }
+    
 }
 
 #Preview {
