93bd9be8831d25cf4a00ed93181caaadf1bcbb9d
Steph W <stephanie.worladge@rmit.edu.au>
2025-02-03 16:49:52 +1100

    Initial Commit

---
+++ NotesPersist/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NotesPersist
+//
+//  Created by Steph on 3/2/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NotesPersist/NotesPersistApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesPersistApp.swift
+//  NotesPersist
+//
+//  Created by Steph on 3/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesPersistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
